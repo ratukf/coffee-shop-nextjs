f86af08a3c4d480316db564d8de372826db35b61
Ratu <ratu@praisindo.co.id>
2024-09-03 17:32:04 +0700

    Add _app.tsx, _document.tsx, index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "coffee/styles/globals.css";
-import type { AppProps } from "next/app";
+import 'coffee/styles/globals.css'
+import type { AppProps } from 'next/app'
+import MainLayout from 'coffee/components/layouts/MainLayout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <MainLayout>
+            <Component {...pageProps} />
+        </MainLayout>
+    )
 }
