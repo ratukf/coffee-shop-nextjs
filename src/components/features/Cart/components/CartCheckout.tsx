410d86313c38e9d9ac340af249cd9bce0b97229c
Ratu <ratu@praisindo.co.id>
2024-09-04 09:56:08 +0700

    Add checkout button

---
+++ src/components/features/Cart/components/CartCheckout.tsx
@@ -0,0 +1,25 @@
+import { useAtom } from 'jotai';
+import { useRouter } from 'next/router';
+
+import { cartAtom } from 'coffee/store/CartAtom';
+
+export default function CartCheckout() {
+    const [cart] = useAtom(cartAtom);
+    const router = useRouter();
+
+    const handleCheckout = () => {
+        if (cart.length === 0) {
+            alert('Your cart is empty')
+            return;
+        }
+    }
+
+    return (
+            <td colSpan={5} className="px-6 py-3 text-xs text-end font-medium text-calm-black tracking-wider">
+                <button onClick={handleCheckout} className="bg-calm-brown text-white font-bold text-xl p-5 rounded shadow-lg">
+                    Checkout
+                </button>
+            </td>
+
+    );
+}
--- src/components/features/Cart/components/CartTable.tsx
+++ src/components/features/Cart/components/CartTable.tsx
@@ -3,30 +3,34 @@ import { useAtom } from 'jotai';
 import { cartAtom } from 'coffee/store/CartAtom';
 import CartItem from 'coffee/components/features/Cart/components/CartItem';
 import CartTotal from 'coffee/components/features/Cart/components/CartTotal';
-import CartQuantity from 'coffee/components/features/Cart/components/CartQuantity';
+import CartCheckout from 'coffee/components/features/Cart/components/CartCheckout';
 
 export default function CartTable() {
     const [cart] = useAtom(cartAtom);
 
-    return(
-        <table className="table-auto w-full">
-            <thead>
-                <tr>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cart.map(item =>
-                    <CartItem key={item.title} item={item} />
-                )}
-            </tbody>
-            <tfoot>
-                <CartTotal />
-            </tfoot>
-        </table>
+    return (
+        <section className="flex justify-center items-center">
+            <table className="table-auto">
+                <thead>
+                    <tr className="border-b-2 border-calm-brown">
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total</th>
+                    </tr>
+                </thead>
+                <tbody className="border-b-2 border-calm-brown">
+                    {cart.map(item =>
+                        <CartItem key={item.title} item={item} />
+                    )}
+                </tbody>
+                <tfoot>
+                    <CartTotal />
+                    <CartCheckout />
+                </tfoot>
+            </table>
+
+        </section>
     )
 }
